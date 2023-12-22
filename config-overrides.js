3a49aa431274c2f934f023b374bfa865c75e13c9
Fateh Ali <fatehalive@gmail.com>
2023-12-22 15:50:06 +0700

    init

---
+++ config-overrides.js
@@ -0,0 +1,28 @@
+const path = require('node:path')
+const appVersion = require(path.join(process.cwd(), 'package.json')).version;
+
+module.exports = {
+    paths: (paths, env) => {
+        console.log("> paths_stage\n")
+        return paths;
+    },
+    jest: config => {
+        console.log("> jest_stage\n")
+        return config;
+    },
+    devServer: configFunction => {
+        console.log("> devServer_stage\n")
+        const config = function(proxy, allowedHost) {
+            return configFunction(proxy, allowedHost)
+        }
+        return config;
+    },
+    webpack: (config, env) => {
+        console.log("> webpack_stage\n")
+
+        /* Your custom webpack here */
+        // config.output.filename = `static/js/[name].${appVersion}-[contenthash].js`
+
+        return config;
+    }
+}
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          v0.1.0
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
